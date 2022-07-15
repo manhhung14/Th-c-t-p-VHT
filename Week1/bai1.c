d6357ca6970bfae6eed70a0930ef0f49c09f2ea3
Nguyen Manh Hung <71164276+manhhung14@users.noreply.github.com>
2022-07-15 10:28:17 +0700

    update

--- Week1/bai1.c
+++ Week1/bai1.c
@@ -29,7 +29,7 @@ void *getTime(void *args )
 {		
 	while (1)
 	{
-		unsigned long x = *((unsigned long*)args);
+		//unsigned long x = *((unsigned long*)args);
 		if (clock_nanosleep(CLOCK_REALTIME,0,&t1,&t2) == 0)
 		{
 			clock_gettime(CLOCK_REALTIME,&tmp1);
@@ -45,14 +45,16 @@ void *getTime(void *args )
 
 void *getFreq(void *args)
 {
+	unsigned long x = *((unsigned long*)args);
 	while(1){
 		
-		unsigned long x = *((unsigned long*)args);
+		
 		unsigned long new_freq = get_freq();
 		
 		if ( new_freq != x )
 		{
 			freq = new_freq;
+			
 			if (freq<1000000000)
 			{
 				t1.tv_sec = 0;
@@ -70,10 +72,13 @@ void *getFreq(void *args)
 
 void *save_time(void *args)
 {
+	tmp1 = *((struct timespec *)args);
+	tmp2.tv_sec = 0;
+	tmp2.tv_nsec = 0; 
 	while(1){
-		struct timespec *tp = (struct timespec *)args;
+		
   
-		if(tmp2.tv_sec != tp->tv_sec | tmp2.tv_nsec != tp->tv_nsec)
+		if(tmp2.tv_sec != tmp1.tv_sec | tmp2.tv_nsec != tmp1.tv_nsec)
 		{
 		FILE *file;
 		file = fopen("time_and_interval.txt","a+");
@@ -83,22 +88,22 @@ void *save_time(void *args)
 		unsigned long sub_nsec;
 
 		
-		if(tp->tv_nsec < tmp2.tv_nsec){
-			sub_nsec = 1000000000 + tp->tv_nsec - tmp2.tv_nsec;
-			sub_sec = tp->tv_sec - tmp2.tv_sec - 1;
+		if(tmp1.tv_nsec < tmp2.tv_nsec){
+			sub_nsec = 1000000000 + tmp1.tv_nsec - tmp2.tv_nsec;
+			sub_sec = tmp1.tv_sec - tmp2.tv_sec - 1;
 
 		}
 		else
 		{
-			sub_nsec =  tp->tv_nsec - tmp2.tv_nsec;
-			sub_sec = tp->tv_sec - tmp2.tv_sec;
+			sub_nsec =  tmp1.tv_nsec - tmp2.tv_nsec;
+			sub_sec = tmp1.tv_sec - tmp2.tv_sec;
 		}
 		
-		fprintf(file,"\n%ld.%09ld\n%ld.%09ld",tp->tv_sec,tp->tv_nsec,sub_sec,sub_nsec); 
+		fprintf(file,"\n%ld.%09ld\n%ld.%09ld",tmp1.tv_sec,tmp1.tv_nsec,sub_sec,sub_nsec); 
 		//fprintf(file,"\n%ld.%09ld",sub_sec,sub_nsec);  
 		fclose(file);
-		tmp2.tv_sec = tp->tv_sec;
-		tmp2.tv_nsec = tp->tv_nsec;
+		tmp2.tv_sec = tmp1.tv_sec;
+		tmp2.tv_nsec = tmp1.tv_nsec;
 		
 		}
 		
@@ -118,8 +123,7 @@ int main(int argc, char const *argv[])
    	t1.tv_nsec = freq; 
 	tmp1.tv_sec = 0;
 	tmp1.tv_sec = 0;
-	tmp2.tv_sec = 0;
-	tmp2.tv_sec = 0;        
+	       
    	a1 = pthread_create(&INPUT,NULL,getFreq,&freq);
     a2 = pthread_create(&SAMPLE, NULL, getTime,&freq);
     a3 = pthread_create(&LOGGING,NULL,save_time,&tmp1);
